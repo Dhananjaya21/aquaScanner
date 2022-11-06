a36aa78fcb9b91f6145ba58d94897150d6989a8e
Dhananjaya21 <idananjaya30@gmail.com>
2022-11-07 02:29:43 +0530

    new

--- blue_gradient_wireframe/lib/AllPages/disease_identification_page.dart
+++ blue_gradient_wireframe/lib/AllPages/disease_identification_page.dart
@@ -33,67 +33,15 @@ class _DiseaseIdentificationPageState extends State<DiseaseIdentificationPage> {
             image: DecorationImage(image: AssetImage("lib/img/3443996.jpg"),fit: BoxFit.cover)
         ),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
           children: [
-            ButtonTheme(
-              minWidth: 200.0,
-                height: 200.0,
-                child: ElevatedButton(
-                  
-                  onPressed: () {
-                    showDialog(
-                        context: context,
-                        builder: (BuildContext context) {
-                          return AlertDialog(
-                            shape:
-                            RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
-                            title: const Text('Please choose media to select'),
-                            content: Container(
-                              height: MediaQuery.of(context).size.height / 6,
-                              child: Column(
-                                children: [
-                                  ElevatedButton(
-                                    //if user click this button, user can upload image from gallery
-                                    onPressed: () {
-                                      Navigator.pop(context);
-                                      getImage(ImageSource.gallery);
-                                    },
-                                    child: Row(
-                                      children: const [
-                                        Icon(Icons.image),
-                                        Text('From Gallery'),
-                                      ],
-                                    ),
-                                  ),
-                                  ElevatedButton(
-                                    //if user click this button. user can upload image from camera
-                                    onPressed: () {
-                                      Navigator.pop(context);
-                                      getImage(ImageSource.camera);
-                                    },
-                                    child: Row(
-                                      children: const [
-                                        Icon(Icons.camera),
-                                        Text('From Camera'),
-                                      ],
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ),
-                          );
-                        });
-                  },
-                  child: Text('Upload Photo'),
-                ),
-            ),
+
             const SizedBox(
-              height: 10,
+              height: 100,
             ),
             //if image not null show the image
-            //if image null show text
-            image != null
-                ? Padding(
+            //if image null show button
+            if (image != null) Padding(
               padding: const EdgeInsets.symmetric(horizontal: 20),
               child: ClipRRect(
                 borderRadius: BorderRadius.circular(8),
@@ -105,11 +53,58 @@ class _DiseaseIdentificationPageState extends State<DiseaseIdentificationPage> {
                   height: 300,
                 ),
               ),
-            )
-                : const Text(
-              "No Image",
-              style: TextStyle(fontSize: 20),
-            )
+            ) else ButtonTheme(
+              minWidth: 200.0,
+              height: 200.0,
+              child: ElevatedButton(
+
+                onPressed: () {
+                  showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return AlertDialog(
+                          shape:
+                          RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+                          title: const Text('Please choose media to select'),
+                          content: Container(
+                            height: MediaQuery.of(context).size.height / 6,
+                            child: Column(
+                              children: [
+                                ElevatedButton(
+                                  //if user click this button, user can upload image from gallery
+                                  onPressed: () {
+                                    Navigator.pop(context);
+                                    getImage(ImageSource.gallery);
+                                  },
+                                  child: Row(
+                                    children: const [
+                                      Icon(Icons.image),
+                                      Text('From Gallery'),
+                                    ],
+                                  ),
+                                ),
+                                ElevatedButton(
+                                  //if user click this button. user can upload image from camera
+                                  onPressed: () {
+                                    Navigator.pop(context);
+                                    getImage(ImageSource.camera);
+                                  },
+                                  child: Row(
+                                    children: const [
+                                      Icon(Icons.camera),
+                                      Text('From Camera'),
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        );
+                      });
+                },
+                child: Text('Upload Photo'),
+              ),
+            ),
           ],
         ),
       ),
