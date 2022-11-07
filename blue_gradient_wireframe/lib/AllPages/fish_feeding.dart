107dc35cc4cc2675a6d75034b224f03cc59ab626
nethmindulsara <dulsaranethmin@gmail.com>
2022-11-07 20:56:20 +0530

    fish photo upload

--- blue_gradient_wireframe/lib/AllPages/fish_feeding.dart
+++ blue_gradient_wireframe/lib/AllPages/fish_feeding.dart
@@ -2,40 +2,106 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:animated_toggle_switch/animated_toggle_switch.dart';
-
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'home_page.dart';
 
 class FishFeeding extends StatefulWidget {
   const FishFeeding({Key? key}) : super(key: key);
-
   @override
   _FishFeedingState createState() => _FishFeedingState();
 }
+
 bool positive = false;
+int val = 0;
+var current_user = FirebaseAuth.instance.currentUser;
 
 class _FishFeedingState extends State<FishFeeding> {
+  //var current_user = FirebaseAuth.instance.currentUser;
+
+  void updateFishFeed() async {
+    Map<String, dynamic> data = {
+      "user": current_user!.uid,
+      "count": val,
+      "toggle_state": positive
+    };
+    print(positive);
+    print(val);
+    await FirebaseFirestore.instance
+        .collection('fishfeed')
+        .doc(current_user!.uid)
+        .set(data);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
         constraints: const BoxConstraints.expand(),
         decoration: const BoxDecoration(
-            image: DecorationImage(image: AssetImage("lib/img/3443970.jpg"),fit: BoxFit.cover)
-        ),
+            image: DecorationImage(
+                image: AssetImage("lib/img/3443970.jpg"), fit: BoxFit.cover)),
         child: SingleChildScrollView(
           child: Column(
-            children:  [
-              const SizedBox(height: 95.0,),
+            children: [
+              const SizedBox(
+                height: 95.0,
+              ),
               const Text(
                   "Click the button \nto turn on the \nautomatic fish feeder.",
                   style: TextStyle(
                       fontSize: 28.0,
                       color: CupertinoColors.white,
-                      fontWeight: FontWeight.bold
-                  ),
-                  textAlign: TextAlign.center
+                      fontWeight: FontWeight.bold),
+                  textAlign: TextAlign.center),
+              const SizedBox(
+                height: 75.0,
               ),
-              const SizedBox(height: 75.0,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  IconButton(
+                    onPressed: () {
+                      setState(() {
+                        val--;
+                        if (val < 0) {
+                          val = 0;
+                        }
+                      });
+                      updateFishFeed();
+                    },
+                    icon: Icon(Icons.horizontal_rule),
+                    color: Color.fromARGB(255, 5, 5, 5),
+                    iconSize: 50,
+                  ),
+                  Container(
+                    child: Center(
+                        child: Text(
+                      '$val',
+                      style: TextStyle(fontSize: 30),
+                    )),
+                    color: Color.fromARGB(255, 228, 226, 226),
+                    width: 60,
+                    height: 60,
+                  ),
+                  IconButton(
+                    onPressed: () {
+                      setState(() {
+                        val++;
+                        if (val > 99) {
+                          val = 99;
+                        }
+                      });
+                      updateFishFeed();
+                    },
+                    icon: Icon(Icons.add),
+                    color: Color.fromARGB(255, 5, 5, 5),
+                    iconSize: 50,
+                  )
+                ],
+              ),
+              SizedBox(
+                height: 20,
+              ),
               AnimatedToggleSwitch<bool>.dual(
                 current: positive,
                 first: false,
@@ -44,7 +110,6 @@ class _FishFeedingState extends State<FishFeeding> {
                 borderColor: Colors.transparent,
                 borderWidth: 5.0,
                 height: 55,
-
                 boxShadow: const [
                   BoxShadow(
                     color: Colors.black26,
@@ -53,42 +118,53 @@ class _FishFeedingState extends State<FishFeeding> {
                     offset: Offset(0, 1.5),
                   ),
                 ],
-                onChanged: (b) => setState(() => positive = b),
+                onChanged: (b) {
+                  setState(() => positive = b);
+                  updateFishFeed();
+                },
                 colorBuilder: (b) => b ? Colors.red : Colors.green,
-
                 textBuilder: (value) => value
-                    ? const Center(child: Text('Off',style: TextStyle(
-                    fontSize: 20.0,
-                    color: CupertinoColors.black,
-                    fontWeight: FontWeight.bold
-                ),))
-                    : const Center(child: Text('On',style: TextStyle(
-                    fontSize: 20.0,
-                    color: CupertinoColors.black,
-                    fontWeight: FontWeight.bold
-                ),)),
+                    ? const Center(
+                        child: Text(
+                        'Off',
+                        style: TextStyle(
+                            fontSize: 20.0,
+                            color: CupertinoColors.black,
+                            fontWeight: FontWeight.bold),
+                      ))
+                    : const Center(
+                        child: Text(
+                        'On',
+                        style: TextStyle(
+                            fontSize: 20.0,
+                            color: CupertinoColors.black,
+                            fontWeight: FontWeight.bold),
+                      )),
               ),
-              const SizedBox(height: 380.0,),
-              Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
+              const SizedBox(
+                height: 340.0,
+              ),
+              Row(mainAxisAlignment: MainAxisAlignment.end,
                   //mainAxisAlignment = MainAxisAlignment.start,
-                  children:   [
+                  children: [
                     IconButton(
                       visualDensity: VisualDensity.standard,
                       iconSize: 25.0,
-                      icon: const Icon(Icons.home,color: CupertinoColors.black,size: 40.0,),
+                      icon: const Icon(
+                        Icons.home,
+                        color: CupertinoColors.black,
+                        size: 40.0,
+                      ),
                       tooltip: 'Increase volume by 10',
                       onPressed: () {
                         Navigator.push(
                           context,
-                          MaterialPageRoute(builder: (context) => const HomePage()),
+                          MaterialPageRoute(
+                              builder: (context) => const HomePage()),
                         );
                       },
                     ),
-                  ]
-              )
-
-
+                  ]),
             ],
           ),
         ),
--- blue_gradient_wireframe/lib/AllPages/signup_page.dart
+++ blue_gradient_wireframe/lib/AllPages/signup_page.dart
@@ -1,5 +1,6 @@
 import 'package:blue_gradient_wireframe/AllPages/login_page.dart';
 import 'package:blue_gradient_wireframe/main.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/cupertino.dart';
@@ -25,29 +26,43 @@ class _SignupPageState extends State<SignupPage> {
   TextEditingController passwordTextEditingController = TextEditingController();
 
   registerNewUser() async {
-
-
     final User? firebaseUser = (await FirebaseAuth.instance
-        .createUserWithEmailAndPassword(
+            .createUserWithEmailAndPassword(
       email: emailTextEditingController.text.trim(),
       password: passwordTextEditingController.text.trim(),
-    ).catchError((msg) {
+    )
+            .catchError((msg) {
       Fluttertoast.showToast(msg: "Error" + msg.toString());
-    })).user;
+    }))
+        .user;
 
     if (firebaseUser != null) {
-      Map userMap = {
+      Map<String, dynamic> userMap = {
         "id": firebaseUser.uid,
         "username": fullNameTextEditingController.text.trim(),
         "email": emailTextEditingController.text.trim(),
         "phone": userNameTextEditingController.text.trim(),
-        "PH value":"5",
-        "water visibility":"not clear",
-        "TDS value":"56",
       };
-      DatabaseReference userRef =
-      FirebaseDatabase.instance.ref().child("users");
-      userRef.child(firebaseUser.uid).set(userMap);
+
+      Map<String, dynamic> water_data = {
+        "id": firebaseUser.uid,
+        "PH value": "5",
+        "water visibility": "not clear",
+        "TDS value": "56",
+      };
+      // DatabaseReference userRef =
+      //     FirebaseDatabase.instance.ref().child("users");
+      // userRef.child(firebaseUser.uid).set(userMap);
+
+      FirebaseFirestore.instance
+          .collection('users')
+          .doc(firebaseUser.uid)
+          .set(userMap);
+
+      FirebaseFirestore.instance
+          .collection('waterquality')
+          .doc(firebaseUser.uid)
+          .set(water_data);
 
       Navigator.push(
         context,
@@ -57,36 +72,40 @@ class _SignupPageState extends State<SignupPage> {
       );
     }
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
         constraints: const BoxConstraints.expand(),
         decoration: const BoxDecoration(
-            image: DecorationImage(image: AssetImage("lib/img/aquarium-iphone-12-pro-max.jpg"),fit: BoxFit.cover)
-        ),
+            image: DecorationImage(
+                image: AssetImage("lib/img/aquarium-iphone-12-pro-max.jpg"),
+                fit: BoxFit.cover)),
         child: SingleChildScrollView(
           child: Column(
-            children:  [
-              const SizedBox(height: 65.0,),
-              const Text(
-                  "Aqua \nScanner",
+            children: [
+              const SizedBox(
+                height: 65.0,
+              ),
+              const Text("Aqua \nScanner",
                   style: TextStyle(
                       fontSize: 64.0,
                       color: CupertinoColors.white,
-                      fontWeight: FontWeight.bold
-                  ),
-                  textAlign: TextAlign.center
+                      fontWeight: FontWeight.bold),
+                  textAlign: TextAlign.center),
+              const SizedBox(
+                height: 100.0,
               ),
-              const SizedBox(height: 100.0,),
-
               Padding(
-
-                padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 5),
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 25, vertical: 5),
                 child: TextField(
                   controller: emailTextEditingController,
                   decoration: const InputDecoration(
-                    border: OutlineInputBorder(borderRadius: BorderRadius.all(Radius.circular(25.0)),),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(25.0)),
+                    ),
                     hintText: 'Email or Phone Number',
                     filled: true,
                     fillColor: CupertinoColors.white,
@@ -99,7 +118,9 @@ class _SignupPageState extends State<SignupPage> {
                   keyboardType: TextInputType.emailAddress,
                   controller: fullNameTextEditingController,
                   decoration: const InputDecoration(
-                    border: OutlineInputBorder(borderRadius: BorderRadius.all(Radius.circular(25.0)),),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(25.0)),
+                    ),
                     hintText: 'Full Name',
                     filled: true,
                     fillColor: CupertinoColors.white,
@@ -107,59 +128,73 @@ class _SignupPageState extends State<SignupPage> {
                 ),
               ),
               Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 5),
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 25, vertical: 5),
                 child: TextField(
                   controller: userNameTextEditingController,
                   decoration: const InputDecoration(
-                    border: OutlineInputBorder(borderRadius: BorderRadius.all(Radius.circular(25.0)),),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(25.0)),
+                    ),
                     hintText: 'Username',
                     filled: true,
                     fillColor: CupertinoColors.white,
                   ),
                 ),
               ),
-
               Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 5),
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 25, vertical: 5),
                 child: TextField(
                   obscureText: true,
                   controller: passwordTextEditingController,
                   decoration: const InputDecoration(
-                    border: OutlineInputBorder(borderRadius: BorderRadius.all(Radius.circular(25.0)),),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(25.0)),
+                    ),
                     hintText: 'Password',
                     filled: true,
                     fillColor: CupertinoColors.white,
                   ),
                 ),
               ),
-              const SizedBox(height: 10.0,),
+              const SizedBox(
+                height: 10.0,
+              ),
               Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 5),
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 25, vertical: 5),
                 child: ElevatedButton(
                   style: ButtonStyle(
-                    shape: MaterialStateProperty.resolveWith<OutlinedBorder>((_) {
-                      return RoundedRectangleBorder(borderRadius: BorderRadius.circular(25));
+                    shape:
+                        MaterialStateProperty.resolveWith<OutlinedBorder>((_) {
+                      return RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(25));
                     }),
-                    backgroundColor: MaterialStateProperty.all(Colors.indigoAccent),
+                    backgroundColor:
+                        MaterialStateProperty.all(Colors.indigoAccent),
                   ),
                   child: Container(
                     height: 50.0,
                     child: const Center(
                       child: Text(
                         "SIGN UP",
-                        style: TextStyle(fontSize: 18.0, fontFamily: "Brand Bold",color: Colors.white),
+                        style: TextStyle(
+                            fontSize: 18.0,
+                            fontFamily: "Brand Bold",
+                            color: Colors.white),
                       ),
                     ),
                   ),
-
-                  onPressed: (){
+                  onPressed: () {
                     registerNewUser();
                   },
                 ),
               ),
               TextButton(
                 style: ButtonStyle(
-                  foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
+                  foregroundColor:
+                      MaterialStateProperty.all<Color>(Colors.white),
                 ),
                 onPressed: () {
                   Navigator.push(
@@ -169,8 +204,9 @@ class _SignupPageState extends State<SignupPage> {
                 },
                 child: const Text('Already have an account.LOG IN'),
               ),
-              const SizedBox(height: 160.0,),
-
+              const SizedBox(
+                height: 160.0,
+              ),
             ],
           ),
         ),
